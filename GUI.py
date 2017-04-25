37454797484f5d68aae59943ec8a3234643cfc2c
Christian Raul Delgado Diaz de Leon <christian.delgado-diaz-de-leon@insa-lyon.fr>
2017-04-25 16:38:23 +0200

    GUI update

--- GUI.py
+++ GUI.py
@@ -1,57 +1,31 @@
-import sys
-import Tkinter as tk
+from Tkinter import * 
 import tkFileDialog as filedialog
-from PIL import Image, ImageTk
-from PyQt4 import QtGui
+import tkFont
+import Image
+
+fenetre = Tk()
+fenetre.title("SYSTEME DE RECONNAISSANCE D'EMPREINTES DIGITALES")
+fenetre.geometry('600x400')
+
+def choose():
+	filepath = filedialog.askopenfilename(title = "Selectionez une image", filetypes = [('png files','.png'),('all files','.*')])
+	img = PhotoImage(file=filepath)
+	photo = img.subsample(4,4)
+	canvas = Canvas(fenetre, width=photo.width(), height = photo.height() + 10)
+	canvas.create_image(0, 10, anchor=NW, image= photo)
+	#canvas.pack()
+	canvas.grid(row = 3, column = 5)
+	fenetre.mainloop()
+
+b1 = Button(text = "Choose", width = 5, height = 1)
+b1.config(command = choose)
+b1.pack(padx = 8, pady = 1)
+b1.grid(row = 0, column = 1)
 
 
-class window(QtGui.QWidget):
-    
-    def __init__(self):
-        super(window, self).__init__()
-        
-        self.initUI()
-        
-    def choose(event):
-        root = tk.Tk()
-        root.withdraw()
-        file_path = filedialog.askopenfilename(title = "Selectionez une image", filetypes = [('png files','.png'),('all files','.*')])
-        image1 = tk.PhotoImage(file = file_path)
-        return image1
-        
+b2 = Button(text = "Run", width = 5, height = 1)
+#b2.config()
+#b2.pack(padx = 15, pady = 1)
+b2.grid(row = 0, column = 6)
 
-    def initUI(self):
-        
-        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
-        
-        self.setToolTip('Presiona <b>Run</b> para lanzar el programa')
-        
-        btn = QtGui.QPushButton('Open', self)
-        btn.setToolTip('Appuyez <b>Open</b> pour charger une empreinte digitale')
-        btn.resize(btn.sizeHint())
-        btn.move(50, 50)  
-        img = btn.clicked.connect(self.choose)        
-
-        while(img != None):
-            canvas = tk.Canvas(root,width=img.width(),height=img.height())
-            canvas.create_image(0,0,anchor='nw',image = img)
-            canvas.pack()
-
-        btn = QtGui.QPushButton('Run', self)
-        btn.setToolTip("Appuyez <b>Run</b> pour lancer l'algorithme")
-        btn.resize(btn.sizeHint())
-        btn.move(150, 50)
-        
-        self.setGeometry(1500, 400, 800, 500)
-        self.setWindowTitle("SYSTEME DE RECONNAISSANCE D'EMPREINTES DIGITALES")    
-        self.show()
-        
-def main():
-    
-    app = QtGui.QApplication(sys.argv)
-    ex = window()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
+fenetre.mainloop()
